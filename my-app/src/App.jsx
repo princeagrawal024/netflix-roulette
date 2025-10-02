0769bd39a721234bb0325efe39af6e443fba322d
Prince_Agrawal <prince.agrawal@tvh.com>
2025-10-02 14:51:38 +0530

    Completed task1 part2 and 3

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,17 +2,34 @@ import { useState } from 'react'
 import './App.css'
 import CounterIndex from './components/Counter/CounterIndex'
 import GenreNavBar from './components/GenreSelect/GenreNavBar'
+import SearchBar from './components/SearchForm/SearchBar';
 
 
 function App() {
-  let genres = ["ALL", "Crime", "Comedy", "Suspense", "ScienceFiction"];
-  let selectedGenre = genres[1];
-  let onSelectCallBack=(genre) => console.log("Selected genre:", genre)
+
+  //NavBar
+  let genres = ["ALL", "DOCUMENTARY", "COMEDY", "HORROR", "CRIME"];
+  const [selectedGenre, setSelectedGenre] = useState(genres[0]);
+
+  let onSelectCallBack = (genre) => {
+    console.log("Selected genre:", genre);
+    setSelectedGenre(genre);
+  }
+
+  //SearchBar
+  let searchQuery = "suspense thriller";
+  let onSearchCallback = (query)=>{console.log("search movie button clicked:", query)};
+
 
   return (
     <>
-      <div>
+      <div className='MoviesSearchBar'>
+        <SearchBar searchQuery={searchQuery} onSearchCallback={onSearchCallback}/>
+      </div>
+      <div className='NaveBar'>
         <GenreNavBar genres={genres} selectedGenre={selectedGenre} onSelectCallBack={onSelectCallBack} />
+      </div>
+      <div className='Counter'>
         <CounterIndex />
       </div>
     </>
---
+++ my-app/src/components/SearchForm/SearchBar.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import '../../styles/searchBar.css';
+
+export default function SearchBar(props) {
+    let inputSearchQuery = props.searchQuery;
+    let onSearchCallback = props.onSearchCallback;
+
+    const [searchQuery, setSearchQuery] = useState(inputSearchQuery);
+
+    return (
+        <div>
+            <nav className="navbar search-navbar">
+                <form className="search-form">
+                    <input
+                        className="search-input"
+                        type="search"
+                        placeholder="What do you want to watch?"
+                        value={searchQuery}
+                        onChange={(e) => setSearchQuery(e.target.value)}
+                        >
+                    </input>
+
+                    <button
+                        className="search-button"
+                        type="submit"
+                        onClick={()=>onSearchCallback(searchQuery)}
+                        >SEARCH
+                    </button>
+                </form>
+            </nav>
+        </div>
+    )
+}
