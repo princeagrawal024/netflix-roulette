bb0f7b9285aee17e8f29aace4bbb0c6c6c286cb2
Prince_Agrawal <prince.agrawal@tvh.com>
2025-11-01 18:19:49 +0530

    Added ypress and a sample test

---
+++ my-app/cypress/e2e/firstTest.cy.ts
@@ -0,0 +1,10 @@
+describe('Counter Component', () => {
+  it('should increment and decrement', () => {
+    cy.visit('http://localhost:5173');
+    cy.get('.counter h2').should('have.text', '100');
+    cy.contains('+').click();
+    cy.get('.counter h2').should('have.text', '101');
+    cy.contains('-').click();
+    cy.get('.counter h2').should('have.text', '100');
+  });
+});
